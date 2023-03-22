7ea4fd45a500564a1e43495de8b9df3db5106173
LeszekWrobel <wplusw@wp.pl>
2023-03-23 00:06:10 +0100

    php bin/console make:admin:crud

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,6 +7,8 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Announcement;
+use App\Entity\User;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -41,6 +43,7 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('Announcement', 'fas fa-list', Announcement::class);
+        yield MenuItem::linkToCrud('User', 'fas fa-list', User::class);
     }
 }
